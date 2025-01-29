0a2b9632dce1924d1bd455c3df2bcb4d3b77a238
potato-panda <kenny@DESKTOP-PFNU76G.>
2025-01-29 13:03:31 -0500

    resolved issue with pagination and jpa

--- server/src/main/java/com/warehouse/server/repositories/AuthorityRestRepository.java
+++ server/src/main/java/com/warehouse/server/repositories/AuthorityRestRepository.java
@@ -1,10 +1,10 @@
 package com.warehouse.server.repositories;
 
 import com.warehouse.server.entities.Authority;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource(collectionResourceRel = "authorities", path = "authorities")
 @PreAuthorize("hasRole('ADMIN')")
-public interface AuthorityRestRepository extends PagingAndSortingRepository<Authority, Long> {}
+public interface AuthorityRestRepository extends JpaRepository<Authority, Long> {}
--- server/src/main/java/com/warehouse/server/repositories/CompanyRestRepository.java
+++ server/src/main/java/com/warehouse/server/repositories/CompanyRestRepository.java
@@ -2,10 +2,9 @@ package com.warehouse.server.repositories;
 
 import com.warehouse.server.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "companies", path = "companies")
 public interface CompanyRestRepository extends
-                                       PagingAndSortingRepository<Company, Long> {
+                                       JpaRepository<Company, Long> {
 }
