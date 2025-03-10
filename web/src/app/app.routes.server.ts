ff3461455ecd5ca8dcc3ab1f6629fe10a9b87954
potato-panda <kenny@DESKTOP-PFNU76G.>
2025-03-10 17:41:23 -0400

    fixed angular build error by increasing budget, implemented dockerfiles and docker compose for deployment

--- web/src/app/app.routes.server.ts
+++ web/src/app/app.routes.server.ts
@@ -2,6 +2,38 @@ import {RenderMode, ServerRoute} from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'customers/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'suppliers/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'contacts/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'products/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'inventories/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'quotations/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'purchaseOrders/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'sites/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
